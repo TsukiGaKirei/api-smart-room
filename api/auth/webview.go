89702983fd3a2593c4788afbe6409b6fc5e5607b
Gemilang <gilgilang15@gmail.com>
2024-01-18 10:44:30 +0700

    order by

--- api/auth/webview.go
+++ api/auth/webview.go
@@ -12,18 +12,18 @@ func GetDataAllRoom(c echo.Context) error {
 	var result schema.ResponseWebView
 	db := database.GetDBInstance()
 	if err := db.Raw(`select r.rid ,r."name"  ,r.ac_temp , r.last_updated , r.ac, r.lamp ,r.door , r.last_updated_by 
-	from rooms r `).Scan(&result.RoomsWebview).Error; err != nil {
+	from rooms r order by r.rid`).Scan(&result.RoomsWebview).Error; err != nil {
 		return echo.ErrInternalServerError
 	}
 
 	if err := db.Raw(`select ur.uid ,ur.rid , ur.distance ,u.threshold as desired_threshold, ur.threshold , ur.last_updated 
 	from users_rooms ur ,users u 
-	where ur.uid = u.uid `).Scan(&result.UserRoomWebView).Error; err != nil {
+	where ur.uid = u.uid order by ur.uid,ur.rid`).Scan(&result.UserRoomWebView).Error; err != nil {
 		return echo.ErrInternalServerError
 	}
 
 	if err := db.Raw(`select u.uid ,u."name" ,u.desired_radius ,u.desired_temp , u.threshold as desired_threshold,u.smart_room_automation , u.last_updated
-	from users u  `).Scan(&result.UserWebView).Error; err != nil {
+	from users u order by u.uid `).Scan(&result.UserWebView).Error; err != nil {
 		return echo.ErrInternalServerError
 	}
 
